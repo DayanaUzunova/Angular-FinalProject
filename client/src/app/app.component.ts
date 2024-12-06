a9d0641d1f7a0529fbd06782d93441f664736bc5
Dayana Uzunova <daqnauzunova@abv.bg>
2024-12-06 21:00:29 +0200

    Updated imports.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
+import { FooterComponent } from './core/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AuthenticateComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'courses-management-system';
+  title = 'client';
 }

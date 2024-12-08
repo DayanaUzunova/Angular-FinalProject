67e837337f666c81080efacc6fed98668177dfc1
Dayana Uzunova <daqnauzunova@abv.bg>
2024-12-08 16:59:18 +0200

    Login/register and authentication added.

---
+++ client/src/app/auth/auth.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent implements OnInit {
+  isAuthenticating = true;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getProfile().subscribe({
+      next: () => {
+        this.isAuthenticating = false;
+      },
+      error: () => {
+        this.isAuthenticating = false;
+      },
+      complete: () => {
+        this.isAuthenticating = false;
+      },
+    });
+  }
+}

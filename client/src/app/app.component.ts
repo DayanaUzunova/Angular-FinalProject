07261d0846511257f96256e224ab40d912728805
Dayana Uzunova <daqnauzunova@abv.bg>
2024-12-08 16:57:46 +0200

    Updated app.component.ts.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
-import { AuthenticateComponent } from './authenticate/authenticate.component';
+import { AuthComponent } from './auth/auth.component';
 import { FooterComponent } from './core/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AuthenticateComponent, FooterComponent ],
+  imports: [RouterOutlet, HeaderComponent, AuthComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
